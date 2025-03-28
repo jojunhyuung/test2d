862c8a8cad02c16c496918c42ef6e9f0ecec632b
jojunhyung <cka830611@naver.com>
2025-03-28 12:10:46 +0900

    Test
    git test

--- Assets/Scripts/Object/TESTGithub.cs
+++ Assets/Scripts/Object/TESTGithub.cs
@@ -7,12 +7,19 @@ public class TESTGithub : MonoBehaviour
 {
     public float Test1 = 5.0f;
     public float Test2 = 5.0f;
+    public float Test3 = 5.0f;
+    public float Test4 = 5.0f;
     public float ppp;
+    public float sum;
 
     public void test()
     {
         ppp = Test1 + Test2 ;
+        ppp2 = Test3 + Test4 ;
+        sum = ppp + ppp2 ;
         Debug.Log(ppp);
+        Debug.Log(ppp2);
+        Debug.Log(sum);
     }
 
     private void Start()
